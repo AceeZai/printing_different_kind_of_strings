5f47d49267cabca4925b09cbda59bb262dc603f6
Acee Zai Mendez <zaiakari7@gmail.com>
2026-03-01 23:40:33 +0800

    Added the first two steps

--- practice.py
+++ practice.py
@@ -1,2 +1,5 @@
 #PRACTICING GIT COMMANDS
 
+#How to add a file on a repository?
+#First, you need to make a repo, make sure that you use a snake coding casing style.
+#Second, you should have your Termux and Quick Edit ready.
