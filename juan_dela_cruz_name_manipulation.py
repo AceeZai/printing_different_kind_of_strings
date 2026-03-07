5a6a29833b8370de950b4fb0d5211912e0863064
Acee Zai Mendez <zaiakari7@gmail.com>
2026-03-08 01:19:16 +0800

    Used lstring() for manipulation.
    gege
    h

--- juan_dela_cruz_name_manipulation.py
+++ juan_dela_cruz_name_manipulation.py
@@ -1,4 +1,6 @@
-#Ask for name with leading spaces.
+#Ask for name with leading spaces. ex; ____ Acee Zai Mendez
 full_name = input("Enter your fullname: ")
-#Print the name
-print(full_name)
+#Use lstrip.(), this function removes spaces in the beginning of a string
+clean_name = full_name.lstrip()
+#Print clean_name
+print(clean_name)
